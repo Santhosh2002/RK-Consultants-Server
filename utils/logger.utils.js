3ab4cac853319963369e6bab9ab28c6f46147871
Sai Santhosh <77265502+Santhosh2002@users.noreply.github.com>
2025-02-22 16:40:15 +0530

    changes

--- utils/logger.utils.js
+++ utils/logger.utils.js
@@ -10,7 +10,7 @@ const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/server.log" }),
+    //new winston.transports.File({ filename: "logs/server.log" }),
   ],
 });
 
