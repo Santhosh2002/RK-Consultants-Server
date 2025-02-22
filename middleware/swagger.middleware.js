4ce1a244ee1d5e08f174ba91eabc486cc59ccb90
Sai Santhosh <77265502+Santhosh2002@users.noreply.github.com>
2025-02-22 16:42:25 +0530

    / route for swagger

--- middleware/swagger.middleware.js
+++ middleware/swagger.middleware.js
@@ -24,8 +24,8 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 const swaggerDocs = (app) => {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("Swagger Docs available at http://localhost:3000/api-docs");
+  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  console.log("Swagger Docs available at http://localhost:3000");
 };
 
 module.exports = swaggerDocs;
